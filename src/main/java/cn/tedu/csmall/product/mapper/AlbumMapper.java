72aba0663ac8f3388cc941077565a52edefdaaff
始终丶如此 <834163474@qq.com>
2023-02-21 18:54:55 +0800

    Initial commit

---
+++ src/main/java/cn/tedu/csmall/product/mapper/AlbumMapper.java
@@ -0,0 +1,66 @@
+package cn.tedu.csmall.product.mapper;
+
+import cn.tedu.csmall.product.pojo.entity.Album;
+import cn.tedu.csmall.product.pojo.vo.AlbumStandardVO;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * 处理相册数据的Mapper接口
+ *
+ * @author java@tedu.cn
+ * @version 0.0.1
+ */
+@Repository
+public interface AlbumMapper {
+
+    /**
+     * 插入相册数据
+     * @param album 相册数据
+     * @return 受影响的行数
+     */
+    int insert(Album album);
+
+    /**
+     * 批量插入相册数据
+     * @param albumList 若干个相册数据的集合
+     * @return 受影响的行数
+     */
+    int insertBatch(List<Album> albumList);
+
+    /**
+     * 根据ID删除相册数据
+     * @param id 尝试删除的相册数据的ID
+     * @return 受影响的行数
+     */
+    int deleteById(Long id);
+
+    /**
+     * 根据若干个ID批量删除相册数据
+     * @param ids 尝试删除的若干个相册数据的ID的数组
+     * @return 受影响的行数
+     */
+    int deleteByIds(Long[] ids);
+
+    /**
+     * 修改相册数据
+     * @param album 封装了被修改的相册数据的ID和新数据的对象
+     * @return
+     */
+    int update(Album album);
+
+    /**
+     * 统计相册数据的数量
+     * @return 相册数据的数量
+     */
+    int count();
+
+    /**
+     * 根据ID查询数据的详情
+     * @param id 相册ID
+     * @return 匹配的相册数据的详情，如果没有匹配的数据，则返回null
+     */
+    AlbumStandardVO getStandardById(Long id);
+
+}
---
+++ src/main/java/cn/tedu/csmallproduct/Jsd2010CsmallProductApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.csmallproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jsd2010CsmallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jsd2010CsmallProductApplication.class, args);
+    }
+
+}
